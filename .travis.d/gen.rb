a3b00cde739b1f6aabadc7c30015d736576a5c85
Michael Eden <themichaeleden@gmail.com>
2017-06-28 18:31:36 -0700

    Started on generating the k8s apps.

--- .travis.d/gen.rb
+++ .travis.d/gen.rb
@@ -1,14 +1,10 @@
 require 'yaml'
+require 'erb'
 
 SOURCE_DIR = File.expand_path(File.dirname(__FILE__))
-TEMPLATES_DIR = File.join SOURCE_DIR, '../templates/'
+TEMPLATES_DIR = File.join SOURCE_DIR, '/templates/'
 
 APP_TEMPLATE = File.join TEMPLATES_DIR, 'app.yaml.erb'
-
-# Global config for domes
-domes = {}
-# Keyed by dome -> app, so apps[:dev][:checkin]
-apps = {}
 
 def basename_no_ext(file)
   File.basename(file, File.extname(file))
@@ -19,8 +15,10 @@ def load_config
   # Go through all the .yaml and .yml files here!
   (Dir['**/*.yaml'] + Dir['**/*.yml'])
     .select { |f| File.file? f }
-    .select { |f| f[0] != '.' }
+    .select { |f| basename_no_ext(f)[0] != '.' }
     .each_with_object({}) do |file, data|
+
+    puts "Parsing #{file}."
 
     dome_name, app_name, overflow = File.split file
 
@@ -29,9 +27,48 @@ def load_config
     end
     app_name = basename_no_ext app_name
 
-    data[dome_name] = {}
-    data[dome_name][app_name] = YAML.load(File.read(file))
-    data[dome_name][app_name]['name'] = app_name
+    app_config = YAML.safe_load(File.read(file))
+    next if app_config['ignore']
+
+    # generate more configs part
+    if app_config['git'].is_a? String
+      remote = app_config['git']
+      app_config['git'] = {}
+      app_config['git']['remote'] = remote
+    end
+    git = app_config['git']['remote']
+    branch = app_config['git']['branch'] || 'master'
+    git_parts = git.split(File::SEPARATOR)
+    repo_name = basename_no_ext(git_parts[-1])
+    org_name = git_parts[-2]
+
+    # get git rev
+    git_rev = app_config['git']['rev']
+    if git_rev.nil?
+      git_rev = `git ls-remote #{git} #{branch}`
+      git_rev = git_rev.strip.split[0]
+    end
+
+    data[dome_name] = {} unless data.key? dome_name
+    data[dome_name][app_name] = app_config
+    data[dome_name][app_name]['shortname'] = app_name
+    data[dome_name][app_name]['git']['user'] = org_name
+    data[dome_name][app_name]['git']['shortname'] = repo_name
+    data[dome_name][app_name]['git']['rev'] = git_rev
     data
+  end
+end
+
+
+load_config.each do |dome_name, apps|
+  puts 'Generating configs...'
+  puts ''
+  dome = {}
+  dome['name'] = dome_name
+  # TODO: get the real value from
+  dome['mongo'] = 'mongodb://mongo'
+  apps.each do |app_name, app|
+    puts '---'
+    puts ERB.new(File.read(APP_TEMPLATE)).result(binding)
   end
 end
