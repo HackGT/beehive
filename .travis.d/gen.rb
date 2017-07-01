027f364b896a6f08e983aec251bd5fc06067d7ea
Michael Eden <themichaeleden@gmail.com>
2017-06-30 17:20:54 -0700

    Gen ingress config and write files.

--- .travis.d/gen.rb
+++ .travis.d/gen.rb
@@ -5,6 +5,7 @@ SOURCE_DIR = File.expand_path(File.dirname(__FILE__))
 TEMPLATES_DIR = File.join SOURCE_DIR, '/templates/'
 
 APP_TEMPLATE = File.join TEMPLATES_DIR, 'app.yaml.erb'
+INGRESS_TEMPLATE = File.join TEMPLATES_DIR, 'ingress.yaml.erb'
 
 def basename_no_ext(file)
   File.basename(file, File.extname(file))
@@ -16,11 +17,14 @@ def load_config
   (Dir['**/*.yaml'] + Dir['**/*.yml'])
     .select { |f| File.file? f }
     .select { |f| basename_no_ext(f)[0] != '.' }
+    .select { |f| basename_no_ext(f)[0] != '.' }
     .each_with_object({}) do |file, data|
 
     puts "Parsing #{file}."
 
     dome_name, app_name, overflow = File.split file
+
+    next if dome_name == 'output'
 
     if app_name.nil? || !overflow.nil?
       raise "Cannot handle more than one folder deep: #{file}"
@@ -59,16 +63,31 @@ def load_config
   end
 end
 
+puts `rm -rf output/*.yaml`
 
-load_config.each do |dome_name, apps|
-  puts 'Generating configs...'
-  puts ''
+biodomes = load_config
+
+biodomes.each do |dome_name, apps|
   dome = {}
   dome['name'] = dome_name
   # TODO: get the real value from
-  dome['mongo'] = 'mongodb://mongo'
+  dome['mongo'] = 'mongo'
+
   apps.each do |app_name, app|
-    puts '---'
-    puts ERB.new(File.read(APP_TEMPLATE)).result(binding)
+    path = "output/#{app_name}-#{dome_name}-deployment.yaml"
+    puts "Writing #{path}."
+
+    File.open path, 'w' do |file|
+      data = ERB.new(File.read(APP_TEMPLATE)).result(binding)
+      file.write(data)
+    end
   end
+end
+
+path = 'output/ingress.yaml'
+puts "Writing #{path}."
+
+File.open path, 'w' do |file|
+  data = ERB.new(File.read(INGRESS_TEMPLATE)).result(binding)
+  file.write(data)
 end
---
+++ output/README.md
@@ -0,0 +1,4 @@
+# DO NOT PUT CONFIGS HERE
+
+This folder is used by travis to put generated files in, it will be ignored!
+
