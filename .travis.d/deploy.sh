0621f2852c5f27d25061a1229b914133a5d48859
Michael Eden <themichaeleden@gmail.com>
2017-07-02 11:46:53 -0700

    Moved secrets to travis proper, installed helm.

--- .travis.d/deploy.sh
+++ .travis.d/deploy.sh
@@ -1,10 +1,36 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-LATEST_STABLE=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+install_kubectl() {
+    LATEST_STABLE=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/${LATEST_STABLE}/bin/linux/amd64/kubectl
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/${LATEST_STABLE}/bin/linux/amd64/kubectl
+    chmod +x kubectl
+    mv kubectl /usr/bin/
+    mkdir ~/.kube
+    echo "${KUBE_CONFIG}" | base64 -d > ~/.kube/config
+    hash -r
+}
 
-chmod +x kubectl
+install_helm() {
+    curl -LO 'https://kubernetes-helm.storage.googleapis.com/helm-v2.5.0-linux-amd64.tar.gz'
+    tar -zxvf 'helm-v2.5.0-linux-amd64.tar.gz'
+    chmod +x linux-amd64/helm
+    mv linux-amd64/helm /usr/bin/
+    hash -r
+    helm init --client-only
+    helm repo update
+}
 
-./kubectl create -f .output -o yaml
+install_kubectl
+install_helm
+
+echo; echo "Checking kubectl connection!"; echo
+kubectl describe services
+
+echo; echo "Checking helm connection!"; echo
+helm list
+
+# don't do this yet!
+# helm install
+# kubectl create -f .output -o yaml
