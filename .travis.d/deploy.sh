9c2b2de3be11a30711f3dd5a06fcc479474a639e
Michael Eden <themichaeleden@gmail.com>
2017-07-02 11:48:55 -0700

    Dont put bins in system directories.

--- .travis.d/deploy.sh
+++ .travis.d/deploy.sh
@@ -6,7 +6,6 @@ install_kubectl() {
     curl -LO https://storage.googleapis.com/kubernetes-release/release/${LATEST_STABLE}/bin/linux/amd64/kubectl
 
     chmod +x kubectl
-    mv kubectl /usr/bin/
     mkdir ~/.kube
     echo "${KUBE_CONFIG}" | base64 -d > ~/.kube/config
     hash -r
@@ -16,7 +15,7 @@ install_helm() {
     curl -LO 'https://kubernetes-helm.storage.googleapis.com/helm-v2.5.0-linux-amd64.tar.gz'
     tar -zxvf 'helm-v2.5.0-linux-amd64.tar.gz'
     chmod +x linux-amd64/helm
-    mv linux-amd64/helm /usr/bin/
+    mv linux-amd64/helm .
     hash -r
     helm init --client-only
     helm repo update
@@ -26,10 +25,10 @@ install_kubectl
 install_helm
 
 echo; echo "Checking kubectl connection!"; echo
-kubectl describe services
+./kubectl describe services
 
 echo; echo "Checking helm connection!"; echo
-helm list
+./helm list
 
 # don't do this yet!
 # helm install
