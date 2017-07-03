9736d589eecb0dc9d764d4259ef7f3e0f90dbef3
Michael Eden <themichaeleden@gmail.com>
2017-07-02 21:10:46 -0700

    Separate dep & service templates, update cloudflare dns in deploy.

--- .travis.d/deploy.rb
+++ .travis.d/deploy.rb
@@ -1,1 +1,74 @@
 require 'kubeclient'
+require 'cloudflare'
+require 'base64'
+require 'yaml'
+
+# TODO: configuration options
+ROOT_HOST = 'hack.gt'
+CLUSTER_IP = '54.164.227.147'
+
+SOURCE_DIR = File.expand_path(File.dirname(__FILE__))
+KUBE_GLOB = File.join SOURCE_DIR, '../.output/kubernetes/*.yaml'
+CF_DNS_CONFIG = File.join SOURCE_DIR, '../.output/cloudflare/dns.yaml'
+
+def set_cloudflare_dns
+  connection = Cloudflare.connect(
+    email: ENV['CLOUDFLARE_EMAIL'],
+    key: ENV['CLOUDFLARE_AUTH']
+  )
+  zone = connection.zones.find_by_id(ENV['CLOUDFLARE_ZONE'])
+  target = YAML.safe_load(CF_DNS_CONFIG)
+
+  # update existing records
+  zone.dns_records.all.each do |dns|
+    next if target[dns.record[:name]].nil?
+    record = target[dns.record[:name]]
+    record['name'] = dns.record[:name]
+    dns.put(record.to_json, content_type: 'application/json')
+    puts 'Changing Cloudflare DNS: #{record.to_json}'
+    target.delete(dns.record[:name])
+  end
+
+  # add the remaining records
+  target.each do |name, record|
+    record['name'] = name
+    zone.dns_records.post(record.to_json, content_type: 'application/json')
+  end
+end
+
+def deploy_kubernetes
+  # configure the client
+  kube_config = YAML.safe_load(Base64.decode64(ENV['KUBE_CONFIG']))
+  config = Kubeclient::Config.new(kube_config, nil)
+  client = Kubeclient::Client.new(
+    config.context.api_endpoint,
+    config.context.api_version,
+    ssl_options: config.context.ssl_options,
+    auth_options: config.context.auth_options
+  )
+
+  Dir[KUBE_GLOB]
+    .map { |f| [YAML.safe_load(f), f] }
+    .each do |(app, f)|
+
+    # create based on the kind of file
+    puts "Deploying #{f}."
+
+    case app['kind'].downcase
+    when 'deployment'
+      client.create_deployment(app)
+    when 'service'
+      client.create_service(app)
+    when 'ingress'
+      client.create_ingress(app)
+    end
+  end
+end
+
+def install_helm_charts
+  # TODO: helm
+end
+
+set_cloudflare_dns
+install_helm_charts
+deploy_kubernetes
--- .travis.d/gen.rb
+++ .travis.d/gen.rb
@@ -1,12 +1,24 @@
 require 'yaml'
 require 'json'
 require 'erb'
+require 'fileutils'
 
 SOURCE_DIR = File.expand_path(File.dirname(__FILE__))
 TEMPLATES_DIR = File.join SOURCE_DIR, '/templates/'
+KUBE_OUT_DIR = File.join SOURCE_DIR, '../.output/kubernetes/'
+CF_OUT_DIR = File.join SOURCE_DIR, '../.output/cloudflare/'
+CONFIG_ROOT = File.join SOURCE_DIR, '..'
+CONFIG_ROOT_LEN = CONFIG_ROOT.split(File::SEPARATOR).length
+YAML_GLOB = ['*.yaml', '*.yml'].map { |f| File.join CONFIG_ROOT, '**', f }
 
-APP_TEMPLATE = File.join TEMPLATES_DIR, 'app.yaml.erb'
+SERVICE_TEMPLATE = File.join TEMPLATES_DIR, 'service.yaml.erb'
+DEPLOYMENT_TEMPLATE = File.join TEMPLATES_DIR, 'deployment.yaml.erb'
 INGRESS_TEMPLATE = File.join TEMPLATES_DIR, 'ingress.yaml.erb'
+
+# TODO: add config steps
+ROOT_HOST = 'hack.gt'
+CLUSTER_IP = '54.164.227.147'
+MONGO_HOSTNAME = 'altered-pug-mongodb'
 
 def basename_no_ext(file)
   File.basename(file, File.extname(file))
@@ -37,6 +49,13 @@ def load_app_data(data, app_config, dome_name, app_name)
               else
                 app_name
               end
+  host = if app_name == :main
+           ROOT_HOST
+         elsif dome_name == 'default'
+           "#{app_name}.#{ROOT_HOST}"
+         else
+           "#{app_name}.#{dome_name}.#{ROOT_HOST}"
+         end
   data[dome_name] = {} unless data.key? dome_name
   data[dome_name]['name'] = dome_name
   data[dome_name]['apps'] = {} unless data[dome_name].key? 'apps'
@@ -46,29 +65,30 @@ def load_app_data(data, app_config, dome_name, app_name)
   data[dome_name]['apps'][app_name]['git']['rev'] = git_rev
   data[dome_name]['apps'][app_name]['shortname'] = shortname
   data[dome_name]['apps'][app_name]['uid'] = "#{shortname}-#{dome_name}"
+  data[dome_name]['apps'][app_name]['host'] = host
   data
 end
 
 # Load all the configuration files!
 def load_config
   # Go through all the .yaml and .yml files here!
-  (Dir['**/*.yaml'] + Dir['**/*.yml'])
+  Dir[*YAML_GLOB]
     .select { |f| File.file? f }
     .reject { |f| basename_no_ext(f)[0] == '.' || f[0] == '.' }
     .map    { |f| [YAML.safe_load(File.read(f)), f] }
     .reject { |y| y[0]['ignore'] }
-    .each_with_object({}) do |app_data, data|
+    .each_with_object({}) do |(app_config, file), data|
 
-    app_config, file = app_data
     puts "Parsing #{file}."
 
-    dome_name, app_name, overflow = File.split file
+    components = file.split(File::SEPARATOR).drop(CONFIG_ROOT_LEN)
+    dome_name, app_name = components
 
-    if file =~ /main\.ya*ml/
+    if dome_name =~ /main\.ya*ml/ && app_name.nil?
       dome_name = 'default'
       app_name = :main
-    elsif app_name.nil? || !overflow.nil?
-      raise "Cannot handle more than one folder deep: #{file}"
+    elsif components.length > 2
+      raise "YAML configs cannot go more than 1 directory deep! #{file}"
     else
       app_name = basename_no_ext app_name
     end
@@ -77,29 +97,75 @@ def load_config
   end
 end
 
-`rm -rf .output/*.yaml`
+# Clear all our previous configuration
+FileUtils.rm_rf [
+  KUBE_OUT_DIR,
+  CF_OUT_DIR
+]
 
+# Make clean dirs
+FileUtils.mkdir [
+  KUBE_OUT_DIR,
+  CF_OUT_DIR
+]
+
+# Load all the new configurations
 biodomes = load_config
 
+# Create all the app's service and deployment conf files.
 biodomes.each do |dome_name, biodome|
-  # TODO: get the real value from
-  biodome['mongo'] = 'altered-pug-mongodb'
+  biodome['mongo'] = MONGO_HOSTNAME
 
   biodome['apps'].each do |app_name, app|
-    path = ".output/#{app_name}-#{dome_name}-deployment.yaml"
+    path = File.join KUBE_OUT_DIR, "#{app_name}-#{dome_name}-deployment.yaml"
     puts "Writing #{path}."
 
     File.open path, 'w' do |file|
-      data = ERB.new(File.read(APP_TEMPLATE)).result(binding)
-      file.write(data)
+      # generate the config
+      data = ERB.new(File.read(DEPLOYMENT_TEMPLATE)).result(binding)
+      # verify it's real YAML
+      yaml = YAML.safe_load(data)
+      file.write(YAML.dump(yaml))
+    end
+
+    path = File.join KUBE_OUT_DIR, "#{app_name}-#{dome_name}-service.yaml"
+    puts "Writing #{path}."
+
+    File.open path, 'w' do |file|
+      # generate the config
+      data = ERB.new(File.read(SERVICE_TEMPLATE)).result(binding)
+      # verify it's real YAML
+      yaml = YAML.safe_load(data)
+      file.write(YAML.dump(yaml))
     end
   end
 end
 
-path = '.output/ingress.yaml'
+# Create the ingress.yaml file
+path = File.join KUBE_OUT_DIR, 'ingress.yaml'
 puts "Writing #{path}."
 
 File.open path, 'w' do |file|
   data = ERB.new(File.read(INGRESS_TEMPLATE)).result(binding)
-  file.write(data)
+  # verify it's real YAML
+  yaml = YAML.safe_load(data)
+  file.write(YAML.dump(yaml))
+end
+
+# Create the cloudflare DNS settings
+dns = biodomes.each_with_object({}) do |(_, biodome), data|
+  biodome['apps'].each_with_object(data) do |(_, app), inner_data|
+    inner_data[app['host']] = {
+      'type' => 'A',
+      'content' => CLUSTER_IP,
+      'proxied' => true
+    }
+    inner_data
+  end
+end
+
+path = File.join CF_OUT_DIR, 'dns.yaml'
+puts "Writing #{path}."
+File.open path, 'w' do |file|
+  file.write(YAML.dump(dns))
 end
