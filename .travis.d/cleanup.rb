0326e65cfe457fde07cbdcc06388535cf57bb1d0
Michael Eden <themichaeleden@gmail.com>
2017-08-28 18:43:33 -0400

    Also remove deployments.

--- .travis.d/cleanup.rb
+++ .travis.d/cleanup.rb
@@ -4,17 +4,27 @@ require 'kubeclient'
 SOURCE_DIR = File.expand_path(File.dirname(__FILE__))
 KUBE_CONFIG = File.join Dir.home, '.kube/config'
 KUBE_GLOB = File.join SOURCE_DIR, '../.output/kubernetes/*.yaml'
+KUBE_API_EXTENSIONS = 'extensions/v1beta1'
+KUBE_API_VERSION = 'v1'
 HERITAGE_TAG = 'Biodomes'
 
 def make_client
   # configure the client
   config = Kubeclient::Config.read(KUBE_CONFIG)
-  Kubeclient::Client.new(
-    config.context.api_endpoint,
-    config.context.api_version,
-    ssl_options: config.context.ssl_options,
-    auth_options: config.context.auth_options
-  )
+  {
+    classic: Kubeclient::Client.new(
+      config.context.api_endpoint,
+      config.context.api_version,
+      ssl_options: config.context.ssl_options,
+      auth_options: config.context.auth_options
+    ),
+    beta: Kubeclient::Client.new(
+      config.context.api_endpoint + '/apis/',
+      KUBE_API_EXTENSIONS,
+      ssl_options: config.context.ssl_options,
+      auth_options: config.context.auth_options
+    )
+  }
 end
 
 def kubernetes_files
@@ -25,18 +35,24 @@ end
 def deployed_services(client)
   client
     .get_services
-    .reject { |s| s.metadata&.labels&.heritage != HERITAGE_TAG }
+    .select { |s| s.metadata&.labels&.heritage == HERITAGE_TAG }
+end
+
+def deployed_deployments(client)
+  client
+    .get_deployments
+    .select { |s| s.metadata&.labels&.heritage == HERITAGE_TAG }
 end
 
 def declared_services(files)
-  files.reject do |(config, _)|
-    config['metadata'] &&
-      config['metadata']['labels'] &&
-      config['metadata']['labels']['heritage'] == HERITAGE_TAG
-  end
+  files.select { |(config, _)| config['kind'] == 'Service' }
 end
 
-def services_to_remove(deployed, declared)
+def declared_deployments(files)
+  files.select { |(config, _)| config['kind'] == 'Deployment' }
+end
+
+def to_remove(deployed, declared)
   deployed_names = Set.new deployed.map { |s| s.metadata.name }
   declared_names = Set.new declared.map { |(s, _)| s['metadata']['name'] }
   deployed_names - declared_names
@@ -45,15 +61,26 @@ end
 def cleanup(dryrun: true)
   client = make_client
   kubes_files = kubernetes_files
-  to_remove = services_to_remove(
-    deployed_services(client),
+
+  servicies_to_remove = to_remove(
+    deployed_services(client[:classic]),
     declared_services(kubes_files)
   )
 
-  to_remove.each do |service|
+  servicies_to_remove.each do |service|
     puts "Deleting service #{service}."
-    client.delete_service service unless dryrun
+    client[:classic].delete_service service unless dryrun
+  end
+
+  deployments_to_remove = to_remove(
+    deployed_deployments(client[:beta]),
+    declared_deployments(kubes_files)
+  )
+
+  deployments_to_remove.each do |deployment|
+    puts "Deleting service #{deployment}."
+    client[:beta].delete_deployment deployment unless dryrun
   end
 end
 
-cleanup dryrun: ENV['DRYRUN'] != '1'
+cleanup dryrun: ENV['DRYRUN'] == '0'
